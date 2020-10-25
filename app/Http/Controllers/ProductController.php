f42bead196335ff685fe95739fa95f86d073f81f
CatherineChernogor <chernogor_k@mail.ru>
2020-10-25 12:04:23 +0800

    some

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,35 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+
 class ProductController extends Controller
 {
     public function show()
     {
         $products = Product::all();
-
-        return view('home', [
-            'products' => $products
-        ]);
-    }
-
-    public function create(Request $request)
-    {
-        $product = new Product();
-        $product->name = $request->name;
-        $product->price = $request->price;
-        $product->picture_url = $request->picture_url;
-        $product->content = $request->content;
-
-        $product->save();
-
-        return redirect('/');
+        return view('home', ['products' => $products]);
     }
 
     public function view($id)
     {
         $product = Product::findOrFail($id);
-        return //$product->name;
-            view('single-product', ['product' => $product]);
+        return view('single-product', ['product' => $product]);
     }
 
+    /* to admin
+        public function create(Request $request)
+        {
+            $product = new Product();
+
+            $product->name = $request->name;
+            $product->price = $request->price;
+            $product->picture_url = $request->picture_url;
+            $product->content = $request->content;
+
+            $product->save();
+
+            return redirect('/');
+        }
+    */
 }
